8ee97cd29e658d1ecbfb15e5eb5d6286a350cefb
davidngo123 <ngodavid215@gmail.com>
2021-03-28 23:37:48 -0700

    secret key test

--- soymate/settings.py
+++ soymate/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DB_PASS')
+SECRET_KEY = 'ac_*(c8d+(aevtysw@@r(902uxxlr18l^jkx5irhl4ufp$kpyg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
