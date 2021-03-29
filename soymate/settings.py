6d9c100c2168d2cda8ea3590fecbea6a82a0d8f4
davidngo123 <ngodavid215@gmail.com>
2021-03-29 00:01:30 -0700

    fixed settings

--- soymate/settings.py
+++ soymate/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [soymate.herokuapp.com]
+ALLOWED_HOSTS = ['soymate.herokuapp.com']
 
 
 # Application definition
