1abb240a7fc73b102f94df12b7aae77dd9af9e30
davidngo123 <ngodavid215@gmail.com>
2021-03-29 00:22:26 -0700

    settings fix

--- soymate/settings.py
+++ soymate/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'e69e54d4e71093b67eee7257c657fc28c02f6f0143f6b0ff'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
