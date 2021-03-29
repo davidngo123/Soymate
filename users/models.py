46683ba70934fd6f2ef09c2e7fdf87c1b97b2e89
davidngo123 <ngodavid215@gmail.com>
2021-03-29 16:28:21 -0700

    added image compression for profile pic

--- users/models.py
+++ users/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image   
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from io import BytesIO
+from django.core.files import File
+import sys
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,11 +13,21 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    # def save(self, *args, **kwargs):
-    #     super(Profile,self).save(*args, **kwargs)
-    #     img = Image.open(self.image.path)
+    def save(self):
+        # Opening the uploaded image
+        im = Image.open(self.image)
 
-    #     if img.height > 400 or img.width > 400:
-    #         output_size = (400, 400)
-    #         img.thumbnail(output_size)
-    #         img.save(self.image.path)
+        output = BytesIO()
+        if im.height > 400 or im.width > 400:
+        # Resize/modify the image
+            im = im.resize((400, 400))
+
+            # after modifications, save it to the output
+            im.save(output, format='JPEG', quality=95)
+            output.seek(0)
+
+            # change the imagefield value to be the newley modifed image value
+            self.image = InMemoryUploadedFile(output, 'ImageField', "%s.jpg" % self.image.name.split('.')[0], 'image/jpeg',
+                                            sys.getsizeof(output), None)
+
+        super(Profile, self).save()
